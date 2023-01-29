427dce0c3bc32b4cb699b9687a7bdc1e42801828
gz <3437705679@qq.com>
2023-01-29 12:11:02 +0800

    fix: 格式化

--- docs/.vitepress/utils/sidebar.ts
+++ docs/.vitepress/utils/sidebar.ts
@@ -5,7 +5,7 @@ export const sidebar = {
       link: 'docs/hello/hello',
       items: [
         { text: 'Hello', link: 'docs/hello/hello' },
-        { text: '常识', link: 'docs/hello/quotations' }
+        { text: '常识', link: 'docs/hello/quotations' },
         { text: '面试话术', link: 'docs/hello/面试话术' }
       ]
     },
@@ -65,7 +65,7 @@ export const sidebar = {
         { text: '数学对象', link: 'docs/javascript/math-object' },
         { text: '异步函数', link: 'docs/javascript/fun-async' },
         { text: '面向对象编程', link: 'docs/javascript/fun-prototype' },
-        { text: 'Class 类的使用', link: 'docs/javascript/fun-class' },
+        { text: 'Class 类的使用', link: 'docsE/javascript/fun-class' },
         { text: 'JavaScript API', link: 'docs/javascript/javascript-api' },
         { text: '生成器', link: 'docs/javascript/generator' },
         { text: 'proxy 代理', link: 'docs/javascript/proxy' }
