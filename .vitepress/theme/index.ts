6cb3820bc1b57e6aad1cffd0a4c30ff759754afb
gz <3437705679@qq.com>
2023-05-20 16:43:37 +0800

    style: layout

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -2,17 +2,16 @@ import DefaultTheme from 'vitepress/theme'
 import FightingDesign from 'fighting-design'
 import 'fighting-design/dist/index.css'
 import './style/index.scss'
-import type {defineComponent} from 'vue'
+import type { defineComponent } from 'vue'
 
 import Chartbed from './components/Chartbed/index.vue'
 import PlayVideo from './components/PlayVideo/index.vue'
 
 export default {
   ...DefaultTheme,
-  enhanceApp({ app}:ReturnType<typeof defineComponent>) {
-    app.component('Chartbed',Chartbed);
-    app.component('Play',PlayVideo);
+  enhanceApp({ app }: ReturnType<typeof defineComponent>) {
+    app.component('Chartbed', Chartbed)
+    app.component('Play', PlayVideo)
     app.use(FightingDesign)
   }
 }
-
