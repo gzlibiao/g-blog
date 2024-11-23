5fd1b71b4126b4b07c7d73b079d7c8d9a3560549
李彪(lib9) <3437705679@qq.com>
2024-11-23 18:54:43 +0800

    🎉🎉🎉 update: 替换索引

--- .vitepress/configs/algolia.ts
+++ .vitepress/configs/algolia.ts
@@ -1,7 +1,10 @@
 export const algolia = {
   appId: '16NA1JC9XO',
   apiKey: '839bf3f7b585e2af3d7bbf10e870f7c5',
-  indexName: 'gsmart',
+  indexName: 'docs',
+  // appId: 'RHX6KGJ4PT',
+  // apiKey: '9ccfcfff5b8b2ca6318229be055a524b',
+  // indexName: 'chocn',
   placeholder: '请输入搜索内容',
   buttonText: '搜索',
   searchParameters: {}
