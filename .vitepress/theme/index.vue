106c946e4822457c65964d74d6260a0b7dd1de21
李彪(lib9) <3437705679@qq.com>
2024-11-25 22:31:30 +0800

    🎉🎉🎉 解决netlify打包失败

--- .vitepress/theme/index.vue
+++ .vitepress/theme/index.vue
@@ -10,7 +10,9 @@
 
     <f-divider margin="32px" color="rgb(224, 224, 224)"></f-divider>
 
-    <f-text center>🎃 对于可控的事情保持警惕 , 对于不可控的事情保持乐观。 </f-text>
+    <f-text center
+      >🎃 对于可控的事情保持警惕 , 对于不可控的事情保持乐观。
+    </f-text>
 
     <f-divider margin="32px" color="rgb(224, 224, 224)"></f-divider>
 
@@ -26,45 +28,101 @@
     <f-text bold>个性标签</f-text>
 
     <iframe
+      id="myTag"
       src="https://gtols.netlify.app/#/tag"
       height="350"
       class="iframe"
       frameborder="0"
     />
+    <div style="height: 350px; width: 100%" />
+
+    <iframe
+      id="myRemark"
+      src="https://hfymark.netlify.app/.netlify/functions/comment"
+      height="500"
+      class="iframeMark"
+      frameborder="0"
+    />
   </f-main>
 </template>
 
-<script></script>
+<script setup>
+  import { onMounted } from 'vue'
+  function restry(iframe) {
+    var retryCount = 0 // 初始化重试次数
+    var maxRetries = 3 // 最大重试次数
+    var timeoutDuration = 5000 // 超时时间，单位毫秒
+    function retryIframeLoad() {
+      if (retryCount < maxRetries) {
+        console.log('重试加载...')
+        const src = iframe.src // 先清空 src，防止重复加载相同内容
+        iframe.src = '' // 先清空 src，防止重复加载相同内容
+        setTimeout(function () {
+          iframe.src = src // 重新加载iframe内容
+        }, 100) // 设置小的延迟，以确保 iframe src 被清空后再重新加载
+        retryCount++
+      } else {
+        console.log('加载失败，超出最大重试次数！')
+      }
+    }
+    // 检测 iframe 是否加载成功
+    iframe.onload = function () {
+      console.log('Iframe 加载成功!')
+    }
+    // 检测超时
+    setTimeout(function () {
+      if (!iframe.contentWindow || !iframe.contentDocument) {
+        console.log('Iframe 加载超时，开始重试...')
+        retryIframeLoad()
+      }
+    }, timeoutDuration) // 设置超时时间
+  }
+  onMounted(() => {
+    var iframe1 = document.getElementById('myTag')
+    var iframe2 = document.getElementById('myRemark')
+    restry(iframe1)
+    restry(iframe2)
+  })
+</script>
 <style scoped>
-.iframe {
-  width: 100%;
-  position: absolute;
-  right: 0;
-  left: 0;
-  @media (min-width: 1024px) {
-    position: static;
+  .iframe {
+    width: 100%;
+    position: absolute;
+    right: 0;
+    left: 0;
+    /* height: 400px; */
+    /* @media (min-width: 1024px) {
+        position: static;
+      } */
   }
-}
-
-.f-main {
-  max-width: 620px;
-  margin: 40px auto;
-  box-sizing: border-box;
-  padding: 40px;
-  padding-bottom: 0;
-  padding-bottom: 350px;
-
-  @media (min-width: 1024px) {
-    padding-bottom: 40px;
+  .iframeMark {
+    /* height: 100vh; */
+    /* margin-top: 30px; */
+    width: 100%;
+    position: absolute;
+    left: 0;
+    right: 0;
   }
-}
 
-.f-button-default {
-  margin-left: 15px;
-}
+  .f-main {
+    max-width: 620px;
+    margin: 40px auto;
+    box-sizing: border-box;
+    padding: 40px;
+    padding-bottom: 0;
+    padding-bottom: 350px;
 
-.f-text {
-  display: block;
-  line-height: 40px;
-}
+    @media (min-width: 1024px) {
+      padding-bottom: 40px;
+    }
+  }
+
+  .f-button-default {
+    margin-left: 15px;
+  }
+
+  .f-text {
+    display: block;
+    line-height: 40px;
+  }
 </style>
